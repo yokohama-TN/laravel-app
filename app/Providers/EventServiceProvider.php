ca12aa2256ee2e7232b906ac236a53b4cc97635d
yokohama0216 <yokohama.learning@gmail.com>
2023-07-31 08:19:06 +0900

    Notifications & Events fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
