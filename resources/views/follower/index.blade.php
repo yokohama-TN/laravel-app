ee0d9c411242df439145d25a20a4fcb0a6b639d8
yokohama0216 <yokohama.learning@gmail.com>
2023-08-08 23:58:28 +0900

    feat : テーブルの項目作成

--- resources/views/follower/index.blade.php
+++ resources/views/follower/index.blade.php
@@ -1,10 +1,20 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <h1 class="mb-4 text-2xl font-bold text-gray-700">User List</h1>
-        <div class="overflow-hidden shadow-sm">
-        @foreach($followed_ids as $followed_id)
-        <div>{{$followed_id}}</div>
-        @endforeach
-        </div>
+        <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">名前</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">作成日</th>
+                    </tr>
+                    <div class="overflow-hidden shadow-sm">
+                    @foreach($followed_ids as $followed_id)
+                    <div>{{$followed_id}}</div>
+                    @endforeach
+                    </div>
+                    </thead>
+                    </table>
     </div>
 </x-app-layout>
